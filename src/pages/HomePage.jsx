de0e591a858d1f50e8b492f1809bc87e3518f2b5
geochrs <geo.christopoulos@hotmail.com>
2024-09-29 22:35:03 +0300

    refactored the homepage hero section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,17 @@
-import PageContent from '../components/content/PageContent';
 import parkImg from '../assets/parkloft.jpg';
+import HeroSection from '../components/content/HeroSection';
 
 export default function HomePage() {
-  return <PageContent title="Welcome to ParkLoft" imageSrc={parkImg} />;
+  return (
+    <HeroSection
+      title="Discover Your Perfect Parking Space with ParkLoft"
+      imageSrc={parkImg}
+    >
+      {' '}
+      <p>
+        ParkLoft helps you find the best parking spots in the city, with ease
+        and efficiency.
+      </p>
+    </HeroSection>
+  );
 }
