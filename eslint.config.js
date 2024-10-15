196e0311aa6cb34c61dc625a68ae4d233a7fb2e4
geochrs <geo.christopoulos@hotmail.com>
2024-10-15 16:13:35 +0300

    fixed the ignore file with correct values and added in eslint node:true for env globals at the backend

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,10 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 export default [
   { ignores: ['dist'] },
   {
+    env: {
+      browser: true,
+      node: true,
+    },
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
