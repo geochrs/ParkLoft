34325c0c473d3f22b28aea4ff7769fe831175770
geochrs <geo.christopoulos@hotmail.com>
2024-10-26 17:19:19 +0300

    added scroll down indicator

--- src/components/content/HeroSection.jsx
+++ src/components/content/HeroSection.jsx
@@ -13,6 +13,22 @@ export default function HeroSection({ title, imageSrc, children }) {
         </div>
       </div>
       <div className={classes.decorBottom}></div>
+      <div className={classes.scrollDown}>
+        <svg
+          width="6"
+          height="39"
+          viewBox="0 0 6 39"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          data-ani="fade-up"
+          style={{ opacity: 1, transform: 'translate(0px, 0px)' }}
+        >
+          <rect width="6" height="6" fill="#5469E7" className={classes.dot1} />
+          <rect y="11" width="6" height="6" fill="#5469E7" className={classes.dot2} />
+          <rect y="22" width="6" height="6" fill="#5469E7" className={classes.dot3} />
+          <rect y="33" width="6" height="6" fill="#5469E7" className={classes.dot4} />
+        </svg>
+      </div>
     </section>
   );
 }
