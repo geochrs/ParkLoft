b463fdb34a2c4601c1cec64744fc54858c8ee419
geochrs <geo.christopoulos@hotmail.com>
2024-10-26 16:15:59 +0300

    created a bottom decor in hero section

--- src/components/content/AboutSection.jsx
+++ src/components/content/AboutSection.jsx
@@ -3,8 +3,10 @@ import classes from './AboutSection.module.css';
 export default function AboutSection({ title, children }) {
   return (
     <section className={classes.section}>
-      <h2 className={classes.h2}>{title}</h2>
-      <div className={classes.content}>{children}</div>
+      <div className={classes.container}>
+        <h2 className={classes.h2}>{title}</h2>
+        <div className={classes.content}>{children}</div>
+      </div>
     </section>
   );
 }
--- src/components/content/HeroSection.jsx
+++ src/components/content/HeroSection.jsx
@@ -3,13 +3,16 @@ import classes from './HeroSection.module.css';
 export default function HeroSection({ title, imageSrc, children }) {
   return (
     <section className={classes.section}>
-      <div className={classes['left-content']}>
-        <h1 className={classes.h1}>{title}</h1>
-        <div className={classes.content}>{children}</div>
+      <div className={classes.container}>
+        <div className={classes['left-content']}>
+          <h1 className={classes.h1}>{title}</h1>
+          <div className={classes.content}>{children}</div>
+        </div>
+        <div className={classes['img-container']}>
+          <img className={classes.img} src={imageSrc} alt={title} />
+        </div>
       </div>
-      <div className={classes['img-container']}>
-        <img className={classes.img} src={imageSrc} alt={title} />
-      </div>
+      <div className={classes.decorBottom}></div>
     </section>
   );
 }
