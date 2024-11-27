d3b6bc136cb302d759354b233546b776da54ae43
geochrs <geo.christopoulos@hotmail.com>
2024-11-28 00:30:28 +0200

    added loader inside the edit form

--- src/components/content/EditProfileForm.jsx
+++ src/components/content/EditProfileForm.jsx
@@ -2,51 +2,64 @@ import { Form, useRouteLoaderData, useNavigation } from 'react-router-dom';
 import classes from './EditProfileForm.module.css';
 import { modalActions } from '../../store/modal';
 import { useDispatch } from 'react-redux';
+import LoadingIndicator from '../layout/LoadingIndicator';
+import { useState, useEffect } from 'react';
 
 export default function EditProfileForm() {
   const data = useRouteLoaderData('profile');
   const navigation = useNavigation();
   const dispatch = useDispatch();
 
-  const isSubmitting = navigation.state === 'submitting';
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
-  const handleAfterSubmit = () => {
-    dispatch(modalActions.closeModal());
-  };
+  useEffect(() => {
+    if (navigation.state === 'submitting') {
+      setIsSubmitting(true);
+    } else if (isSubmitting && navigation.state === 'idle') {
+      setTimeout(() => {
+        dispatch(modalActions.closeModal());
+        setIsSubmitting(false);
+      }, 1000);
+    }
+  }, [navigation.state, isSubmitting, dispatch]);
 
   return (
-    <Form
-      method="put"
-      className={classes.form}
-      action="/profile"
-      onSubmit={handleAfterSubmit}
-    >
+    <Form method="put" className={classes.form} action="/profile">
       <h2 className={classes.title}>Edit Profile</h2>
-      <div className={classes.inputGroup}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          name="username"
-          defaultValue={data.username}
-        />
-      </div>
+      {isSubmitting && <LoadingIndicator />}
+      {!isSubmitting && (
+        <>
+          <div className={classes.inputGroup}>
+            <label htmlFor="username">Username</label>
+            <input
+              id="username"
+              type="text"
+              name="username"
+              defaultValue={data.username}
+            />
+          </div>
 
-      <div className={classes.inputGroup}>
-        <label htmlFor="phone">Phone</label>
-        <input id="phone" type="tel" name="phone" defaultValue={data.phone} />
-      </div>
+          <div className={classes.inputGroup}>
+            <label htmlFor="phone">Phone</label>
+            <input
+              id="phone"
+              type="tel"
+              name="phone"
+              defaultValue={data.phone}
+            />
+          </div>
 
-      <div className={classes.inputGroup}>
-        <label htmlFor="dateOfBirth">Date of Birth</label>
-        <input
-          id="dateOfBirth"
-          type="date"
-          name="dateOfBirth"
-          defaultValue={data.dateOfBirth}
-        />
-      </div>
-
+          <div className={classes.inputGroup}>
+            <label htmlFor="dateOfBirth">Date of Birth</label>
+            <input
+              id="dateOfBirth"
+              type="date"
+              name="dateOfBirth"
+              defaultValue={data.dateOfBirth}
+            />
+          </div>
+        </>
+      )}
       <div className={classes.actions}>
         <button type="submit" disabled={isSubmitting}>
           {isSubmitting ? 'Saving...' : 'Save Changes'}
