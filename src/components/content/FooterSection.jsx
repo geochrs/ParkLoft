f2b00f3a15edc94ebcab882f94b7f91a504217e0
geochrs <geo.christopoulos@hotmail.com>
2024-12-06 17:51:40 +0200

    added content to footer pt1

--- src/components/content/FooterSection.jsx
+++ src/components/content/FooterSection.jsx
@@ -5,13 +5,23 @@ export default function FooterSection() {
   return (
     <section className={classes.section}>
       <div className={classes.container}>
-        <div>
+        <div className={classes.flexbox}>
           <div>
             <img src={logo} className={classes.logo} />
             <p className={classes.content}>
               Discover Your Perfect Parking Space with ParkLoft <br /> Copyright
               © 2024 - All rights reserved
             </p>
+          </div>
+          <div>
+            <div>
+              <h5>Links</h5>
+            </div>
+            <div className={classes.links}>
+              <a>Login</a>
+              <a>Support</a>
+              <a>Documentation</a>
+            </div>
           </div>
         </div>
       </div>
