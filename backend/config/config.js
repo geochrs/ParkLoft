df9b0dfda046297c26b67d4166ccf82a7d712003
geochrs <geo.christopoulos@hotmail.com>
2024-12-28 00:49:36 +0200

    changed to online db

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,10 +1,19 @@
 import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
+import fs from 'fs';
+
 
 dotenv.config();
+const caCertificate = fs.readFileSync(process.env.CA, 'utf8');
 
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      ca: caCertificate,
+      rejectUnauthorized: true,
+    },
+  },
   logging: false,
 });
 
