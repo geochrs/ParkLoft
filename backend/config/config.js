194d9c960fa86e72062910032b7db689f58d79a2
geochrs <geo.christopoulos@hotmail.com>
2024-12-30 12:34:26 +0200

    removed cert from public

--- backend/config/config.js
+++ backend/config/config.js
@@ -2,15 +2,17 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 import fs from 'fs';
 
-
 dotenv.config();
-const caCertificate = fs.readFileSync(process.env.CA, 'utf8');
+const caCertificate = process.env.CERTIFICATE_TIDB;
+const formattedCertificate = `-----BEGIN CERTIFICATE-----\n${caCertificate
+  .match(/.{1,64}/g)
+  .join('\n')}\n-----END CERTIFICATE-----`;
 
-const sequelize = new Sequelize(process.env.DB_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'mysql',
   dialectOptions: {
     ssl: {
-      ca: caCertificate,
+      ca: formattedCertificate,
       rejectUnauthorized: true,
     },
   },
